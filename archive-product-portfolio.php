0022c3117ff0541a64382f1f75191b79144628b6
Slava Sidorov <sidorov-vv3@mail.ru>
2025-07-30 00:24:23 +0500

    путь до картинки подправил

--- archive-product-portfolio.php
+++ archive-product-portfolio.php
@@ -265,24 +265,30 @@
 				<img src="<?php echo get_template_directory_uri(); ?>/img/ico/section-title-dec.svg" class="mb-5">	
 			</div>
 		</div>
-		<div class="row text-start">
+		<div class="row text-start mb-5">
 			<div class="col-md-4">
 				<a href="#" class="portfolio-item-link" data-bs-toggle="modal" data-bs-target="#orderModal">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/actions/new-action-1" class="img-fluid rounded mb-3" alt="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/actions/new-action-1.jpg" class="img-fluid rounded mb-3" alt="">
 				</a>
 			</div>
 			<div class="col-md-4">
 				<a href="#" class="portfolio-item-link" data-bs-toggle="modal" data-bs-target="#orderModal">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/actions/new-action-2" class="img-fluid rounded mb-3" alt="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/actions/new-action-2.jpg" class="img-fluid rounded mb-3" alt="">
 				</a>
 			</div>
 			<div class="col-md-4">
 				<a href="#" class="portfolio-item-link" data-bs-toggle="modal" data-bs-target="#orderModal">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/actions/new-action-3" class="img-fluid rounded mb-3" alt="">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/actions/new-action-3.jpg" class="img-fluid rounded mb-3" alt="">
 				</a>
 			</div>
-			<a href="/акции" type="button" class="btn btn-lg btn-corporate-color-1">Смотреть все акции</a>
 		</div>
+
+		<div class="row text-md-center">
+			<div class="col">
+				<a href="/акции" class="btn btn-lg btn-corporate-color-1 mb-5">Смотреть все акции</a>
+			</div>
+		</div>
+
 		
 		<div class="row">
 			<div class="col text-md-center">
