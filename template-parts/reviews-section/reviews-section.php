ba00415bdc79a7c8e1cb5683b37876fe01535cea
Slava Sidorov <sidorov-vv3@mail.ru>
2025-07-31 00:37:06 +0500

    подправил стили

--- template-parts/reviews-section/reviews-section.php
+++ template-parts/reviews-section/reviews-section.php
@@ -19,7 +19,7 @@
         <div class="row">
             <div class="col text-md-center pb-4">
                 <img src="<?php echo get_template_directory_uri(); ?>/img/ico/yandex-logo.svg" class="mb-3">	
-                <div class="review-rating mb-3 d-flex align-items-end justify-content-center gap-4">
+                <div class="review-rating mb-3 d-flex align-items-md-end justify-content-md-center gap-4">
                     <div>
                         <i class="star-filled"><img src="<?php echo get_template_directory_uri(); ?>/img/ico/stars.svg" alt="stars"></i>
                         <i class="star-filled"><img src="<?php echo get_template_directory_uri(); ?>/img/ico/stars.svg" alt="stars"></i>
