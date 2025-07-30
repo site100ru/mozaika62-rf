c905babbd2bf74b365c4a0ee2febb0b2367e86b9
Slava Sidorov <sidorov-vv3@mail.ru>
2025-07-30 23:19:14 +0500

    подправил ХК

--- archive-actions.php
+++ archive-actions.php
@@ -251,7 +251,7 @@
 		<div class="row">
 			<div class="col">
 				<nav class="breadcrumbs pt-4">
-					<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/ico/breadcrumbs-icon.svg"></a> / 
+					<a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/ico/breadcrumbs-icon.svg"></a> / 
 					<a href="/акции" style="text-decoration: none;">Акции</a>
 				</nav>
 			</div>
