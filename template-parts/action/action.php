dbf9b34decb19821fca55605817e5efa3d4a0493
Slava Sidorov <sidorov-vv3@mail.ru>
2025-10-07 13:03:54 +0500

    closets

--- template-parts/action/action.php
+++ template-parts/action/action.php
@@ -21,7 +21,7 @@ $actions_data = [
         'image_path' => get_template_directory_uri() . '/img/actions/new/new-action-6.webp',
         'alt_text' => 'Скидка 5% На все кухни CUCINA',
         'order' => 1,
-        'category' => 'kitchen, closets, home, cucina',
+        'category' => 'kitchen, home, cucina',
         'modal_data' => 'discount-5',
         'description' => 'Скидка 5% На все кухни CUCINA до 31 октября'
     ],
