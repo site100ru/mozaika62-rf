a7c188ac392ab22a27c49b2c1fbf46fd7e79d777
Slava Sidorov <sidorov-vv3@mail.ru>
2025-10-07 13:02:59 +0500

    closets

--- woocommerce/archive-closets.php
+++ woocommerce/archive-closets.php
@@ -381,7 +381,7 @@
 get_template_part('template-parts/action/action', '', [
     'show_button' => 'true',
     'show_breadcrumbs' => 'false', 
-    'actions_count' => '5',
+    'actions_count' => '3',
     'bg_color' => 'bg-light',
     'section_title' => 'Наши акции',
     'card_type' => 'modal',
