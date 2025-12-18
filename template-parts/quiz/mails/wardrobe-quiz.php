dceeae48d3bd10ee79d091d76f16a03503b1da0c
Slava Sidorov <sidorov-vv3@mail.ru>
2025-12-19 01:15:43 +0500

    почта на мою

--- template-parts/quiz/mails/wardrobe-quiz.php
+++ template-parts/quiz/mails/wardrobe-quiz.php
@@ -70,7 +70,8 @@ $email_body = "
 if ($_POST && $phone) {
     // Отправляем письмо
     mail(
-        "garantshkaf@mail.ru, vasilyev-r@mail.ru",
+        // "garantshkaf@mail.ru, vasilyev-r@mail.ru",
+        "sidorov-vv3@mail.ru",
         "Заявка с Квиза (Шкаф) с сайта мозаика62.рф",
         $email_body,
         $headers
