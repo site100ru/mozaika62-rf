03be6df64b8e4fa8ce44d5742f05de10307750bf
Slava Sidorov <sidorov-vv3@mail.ru>
2026-01-13 02:14:53 +0500

    паралакс

--- js/theme.js
+++ js/theme.js
@@ -3,6 +3,7 @@ $(window).scroll(function (e) {
     var scrolled = $(window).scrollTop();
     $('.parallax-home-section').css('top', (-(scrolled * .35)) + 'px'); // 35 - скорость прокрутки
     $('.parallax-home-section-closets').css('top', (-(scrolled * .35)) + 'px'); // 35 - скорость прокрутки
+    $('.index-parallax-home-section').css('top', (-(scrolled * .35)) + 'px'); // 35 - скорость прокрутки
 });
 /* End parallax home section */
 
