4e1536f4ed1625fdaf9390b107416676680dc159
Roman <150604774+vasilyev-r@users.noreply.github.com>
2026-02-06 18:44:36 +0300

    New (#23)
    * Test
    
    * Update robots func

--- functions.php
+++ functions.php
@@ -517,7 +517,8 @@ function custom_robots_txt($output)
     $output .= "Disallow: *?add-to-cart=*\n";
 	$output .= "Disallow: *?filter_*\n";
 	$output .= "Disallow: *filter_*\n";
-	$output .= "Disallow: */page/*\n";
+	$output .= "Disallow: */page/*\n\n";
+	$output .= "https://мозаика62.рф/sitemap.xml"
     return $output;
 }
 /*** END ДЕЛАЕМ ФАЙЛ ROBOTS.TXT ***/
