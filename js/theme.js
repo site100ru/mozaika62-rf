7d921ae4af6952bc126c0ac4708d698f808ee450
Slava Sidorov <sidorov-vv3@mail.ru>
2026-02-10 01:22:12 +0500

    пробуем паралакс

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,9 @@
 /* Parallax home section */
 $(window).scroll(function (e) {
     var scrolled = $(window).scrollTop();
-    $('.parallax-home-section').css('top', (-(scrolled * .35)) + 'px'); // 35 - скорость прокрутки
-    $('.parallax-home-section-closets').css('top', (-(scrolled * .35)) + 'px'); // 35 - скорость прокрутки
-    $('.index-parallax-home-section').css('top', (-(scrolled * .35)) + 'px'); // 35 - скорость прокрутки
+    $('.parallax-home-section').css('top', (-(scrolled * .25)) + 'px'); // 25 - скорость прокрутки
+    $('.parallax-home-section-closets').css('top', (-(scrolled * .25)) + 'px'); // 25 - скорость прокрутки
+    $('.index-parallax-home-section').css('top', (-(scrolled * .25)) + 'px'); // 25 - скорость прокрутки
 });
 /* End parallax home section */
 
